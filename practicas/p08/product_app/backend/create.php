12c9f02191e64d4133164ee60b19e401536c72d0
uriel <urielsalimora@gmail.com>
2023-10-16 15:51:06 -0600

    archivo read.php corregido

--- practicas/p08/product_app/backend/create.php
+++ practicas/p08/product_app/backend/create.php
@@ -5,12 +5,27 @@
     $producto = file_get_contents('php://input');
     if(!empty($producto)) {
         // SE TRANSFORMA EL STRING DEL JASON A OBJETO
-        $jsonOBJ = json_decode($producto);
-        /**
-         * SUSTITUYE LA SIGUIENTE LÍNEA POR EL CÓDIGO QUE REALICE
-         * LA INSERCIÓN A LA BASE DE DATOS. COMO RESPUESTA REGRESA
-         * UN MENSAJE DE ÉXITO O DE ERROR, SEGÚN SEA EL CASO.
-         */
-        echo '[SERVIDOR] Nombre: '.$jsonOBJ->nombre;
+        $jsonOBJ = json_decode($producto, true);
+
+        $nombre = $jsonOBJ["nombre"];
+        $marca = $jsonOBJ["marca"];
+        $modelo = $jsonOBJ["modelo"];
+        $precio = $jsonOBJ["precio"];
+        $detalles = $jsonOBJ["detalles"];
+        $unidades = $jsonOBJ["unidades"];
+        $imagen = $jsonOBJ["imagen"];
+
+        $consultaObjs = $conexion->query("SELECT * FROM productos WHERE nombre='{$nombre}' AND eliminado=0");
+        if ($consultaObjs->num_rows !=0){
+            echo "El producto ya existe";
+        }
+        else { if ($result = $conexion->query("INSERT INTO productos VALUES(NULL, '{$nombre}', '{$marca}', '{$modelo}', '{$precio}', '{$detalles}', '{$unidades}', '{$imagen}',0) ")){
+            echo "Este producto se ha agregado correctamente";
+        }
+        else{die("Query error: ".mysqli_error($conexion));
+            echo "Este producto no fue agregado";
+        }
+    }$conexion->close();
+        
     }
 ?>
--- practicas/p08/product_app/backend/read.php
+++ practicas/p08/product_app/backend/read.php
@@ -1,29 +1,32 @@
-  <?php
-    include_once __DIR__.'/database.php';
+<?php
+include_once __DIR__ . '/database.php';
 
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}' or nombre like '%{$id}%' or marca like '%{$id}%' or detalles like '%{$id}%'") ) {
-            // SE OBTIENEN LOS RESULTADOS
-			$row = $result->fetch_array(MYSQLI_ASSOC);
-
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = utf8_encode($value);
+// SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+$data = array();
+// SE VERIFICA HABER RECIBIDO EL ID
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+    if ($result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}' || nombre like '{$id}%' || marca like '{$id}%' || detalles like '{$id}%'")) {
+        // SE OBTIENEN LOS RESULTADOS
+        //$row = $result->fetch_array(MYSQLI_ASSOC);
+        $data = array();
+        if (!is_null($result)) {
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $productData = array();
+                // Codifica a UTF-8 los datos y mapea al arreglo de respuesta
+                foreach ($row as $key => $value) {
+                    $productData[$key] = utf8_encode($value);
                 }
+                $data[] = $productData;
             }
-			$result->free();
-		} else {
-            die('Query Error: '.mysqli_error($conexion));
         }
-		$conexion->close();
-    } 
-    
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
-?> 
+        $result->free();
+    } else {
+        die('Query Error: ' . mysqli_error($conexion));
+    }
+    $conexion->close();
+}
+
+// SE HACE LA CONVERSIÓN DE ARRAY A JSON
+echo json_encode($data, JSON_PRETTY_PRINT);
